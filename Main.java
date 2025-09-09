b000ad004c6a1a9c0d0adb291eae76f75c91143e
Paola-commits <paolaamadorys@gmail.com>
2025-09-08 19:54:32 -0600

    Subida inicial del proyecto Java para gestión de estudiantes
    Incluye los archivos Estudiante.java y Main.java, desarrollados en Bloc de notas y validados en JDoodle. Proyecto orientado a objetos con métodos y constructor.

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double[] notas = {9.0, 8.5, 10.0};
+        Estudiante estudiante1 = new Estudiante("Juan Pérez", 20, notas);
+        estudiante1.mostrarDatos();
+    }
+}
